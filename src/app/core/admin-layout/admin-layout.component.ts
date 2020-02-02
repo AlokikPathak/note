1014f17edafa481c6048d9fc08b2b8fd8541a683
alokikp@mindfiresolutions.com <alokikp@mindfiresolutions.com>
2020-02-02 19:39:23 +0530

    link app with new note model

--- src/app/core/admin-layout/admin-layout.component.ts
+++ src/app/core/admin-layout/admin-layout.component.ts
@@ -1,6 +1,6 @@
 import {MediaMatcher} from '@angular/cdk/layout';
 import {ChangeDetectorRef, Component, OnInit, OnDestroy} from '@angular/core';
-
+import {Note} from './../../shared/models/note-model';
 
 @Component({
   selector: 'app-admin-layout',
@@ -11,9 +11,15 @@ export class AdminLayoutComponent implements OnInit, OnDestroy {
 
   mobileQuery: MediaQueryList;
   // Store Side-Nav-Bar content list(Notes)
-  sideNavContent = ["Note1", "Note2", "Note3"];
+  sideNavContent: Note[] = [ 
+    {note_id: 1, user_id: 1, title: "TODO", note : "NOte1", created_on: "02/02/2020 07:12:00", modified_on: "02/02/2020 07:12:00"},
+    {note_id: 2, user_id: 1, title: "TORead", note : "NOte2", created_on: "02/02/2020 07:12:00", modified_on: "02/02/2020 07:12:00"},
+    {note_id: 3, user_id: 1, title: "Courses", note : "NOte3", created_on: "02/02/2020 07:12:00", modified_on: "02/02/2020 07:12:00"},
+  ];
+
   // Store Content of selected Note item
-  noteContent = ["TODO List: Build Angular webapp for notes"];
+  noteContent: Note = 
+    {note_id: 1, user_id: 1, title: "TODO", note : "Note contenttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt", created_on: "02/02/2020 07:12:00",modified_on: "02/02/2020 07:12:00"};
 
   private _mobileQueryListener: () => void;
 
@@ -25,12 +31,8 @@ export class AdminLayoutComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {}
 
-
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
-
-  shouldRun = true;
-  //shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 
 }
